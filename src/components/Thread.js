4057b5564c5c3d2afe87b09f04199697097e6fac
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 13:55:49 -0800

    add prop types

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,14 +1,21 @@
+import PropTypes from "prop-types";
 
 function Thread(props) {
-  const activePercent = props.online / props.memebrs
+  const activePercent = props.accountsActive / props.subscribers
   return (
     <>
       <h3>Subreddit name: {props.displayName} </h3>
       <h3>Members: {props.subscribers}</h3>
-      <h3>Online: {props.accounsActive}</h3>
+      <h3>Online: {props.accountsActive}</h3>
       <h4>Active: {activePercent}% </h4>
     </>
   );
+}
+
+Thread.propTypes = {
+  displayName: PropTypes.string,
+  subscribers: PropTypes.number,
+  accountsActive: PropTypes.number,
 }
 
 export default Thread;
