d8d85793e4f0c4362df8e0a288e90aa0090c8f37
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 15:53:11 -0800

    update react syntax

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,14 +1,16 @@
+import React from "react";
 import PropTypes from "prop-types";
+
 
 function Thread(props) {
   const activePercent = props.accountsActive / props.subscribers
   return (
-    <>
+    <React.Fragment>
       <h3>Subreddit name: {props.displayName} </h3>
       <h3>Members: {props.subscribers}</h3>
       <h3>Online: {props.accountsActive}</h3>
       <h4>Active: {activePercent}% </h4>
-    </>
+    </React.Fragment>
   );
 }
 
