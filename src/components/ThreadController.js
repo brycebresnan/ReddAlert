fa4e97116253e0a0d58cc829c77bb3f1b5f47108
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 13:55:41 -0800

    create react class template

--- src/components/ThreadController.js
+++ src/components/ThreadController.js
@@ -1,5 +1,21 @@
+import ThreadList from "./ThreadList"
+import Thread from "./Thread";
+import React from 'react';
 
-class ThreadController extends React.Component() {
-  return(
-  )
+class ThreadController extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <ThreadList />
+      </React.Fragment>
+    );
+  }
 }
+
+export default ThreadController;
