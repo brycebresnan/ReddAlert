3f4ed89523cc3170063d37af3e2f888c5e36a8ed
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 14:10:49 -0800

    create control for showing form

--- src/components/ThreadController.js
+++ src/components/ThreadController.js
@@ -1,18 +1,28 @@
 import ThreadList from "./ThreadList"
 import Thread from "./Thread";
 import React from 'react';
+import NewThreadForm from "./NewThreadForm";
 
 class ThreadController extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
   }
 
   render(){
+    let currentlyVisisbleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisisbleState = <NewThreadForm />
+    } else {
+      currentlyVisisbleState = <ThreadList />
+    }
+  
     return(
       <React.Fragment>
-        <ThreadList />
+        {currentlyVisisbleState}
       </React.Fragment>
     );
   }
