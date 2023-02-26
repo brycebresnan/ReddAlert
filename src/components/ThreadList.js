a1e63cc0447ea2b2a6b810cb72eb708df7ee718a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 15:52:16 -0800

    prepare for prop list import

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -1,31 +1,24 @@
+import React from "react";
 import Thread from "./Thread";
+import PropTypes from "react";
 
-function ThreadList() {
-
-  const mainThreadList =[
-    {
-      displayName: "Fun Cats!",
-      accountsActive: 1000,
-      subscribers: 4000
-    },
-    {
-      displayName: "Cool Pics",
-      accountsActive: 90,
-      subscribers: 27516
-    }
-  ];
+function ThreadList(props) {
 
   return (
-    <>
-    {mainThreadList.map((thread, index) => 
+    <React.Fragment>
+    {props.threadList.map((thread, index) => 
       <Thread displayName={thread.displayName}
       accountsActive={thread.accountsActive}
       subscribers={thread.subscribers}
       key={index} />
       )}
-    </>
+    </React.Fragment>
 
   );
 }
+
+ThreadList.propTypes = {
+  threadList: PropTypes.array
+};
 
 export default ThreadList;
