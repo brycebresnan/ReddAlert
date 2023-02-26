033dcb85b1ce1c1e00f253eafa55c4cc514e9bbb
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 14:09:49 -0800

    use map to display mutliple threads

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -1,10 +1,30 @@
 import Thread from "./Thread";
 
 function ThreadList() {
+
+  const mainThreadList =[
+    {
+      displayName: "Fun Cats!",
+      accountsActive: 1000,
+      subscribers: 4000
+    },
+    {
+      displayName: "Cool Pics",
+      accountsActive: 90,
+      subscribers: 27516
+    }
+  ];
+
   return (
     <>
-      <Thread />
+    {mainThreadList.map((thread, index) => 
+      <Thread displayName={thread.displayName}
+      accountsActive={thread.accountsActive}
+      subscribers={thread.subscribers}
+      key={index} />
+      )}
     </>
+
   );
 }
 
