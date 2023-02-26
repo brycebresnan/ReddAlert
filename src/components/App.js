ff20bd0643f668e5258a6fcb3429031577f6040a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-26 13:56:13 -0800

    update react syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import Header from "./Header";
+import ThreadController from "./ThreadController";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Header />
-    </>
+      <ThreadController />
+    </React.Fragment>
   );
 }
 
