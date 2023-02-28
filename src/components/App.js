db6653e7daf872e19ed62bcaeee89573c0f54f4a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-27 19:40:59 -0800

    import api component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import ThreadController from "./ThreadController";
+import SubredditData from "./SubredditData";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <SubredditData />
       <ThreadController />
     </React.Fragment>
   );
