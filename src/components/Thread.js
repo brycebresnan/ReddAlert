36ca5dc239218682cbdd3b841c6eb8024c01ba9f
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-03 22:54:59 -0800

    format activity score

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 
 
 function Thread(props) {
-  const activePercent = props.accountsActive / props.subscribers
+  const activePercent = (props.accountsActive / props.subscribers) * 10000
   return (
     <React.Fragment>
       <h3>Subreddit name: {props.displayName} </h3>
       <h3>Members: {props.subscribers}</h3>
       <h3>Online: {props.accountsActive}</h3>
-      <h4>Active: {activePercent}% </h4>
+      <h4>Active Score: {activePercent.toFixed(1)} </h4>
+      <br></br>
     </React.Fragment>
   );
 }
@@ -18,7 +19,7 @@ Thread.propTypes = {
   displayName: PropTypes.string,
   subscribers: PropTypes.number,
   accountsActive: PropTypes.number,
-  id: PropTypes.string
+  id: PropTypes.number
 }
 
 export default Thread;
