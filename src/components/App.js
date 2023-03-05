5b69e3e5993b9a0cddc36f12eda18f9e06b6a07f
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-04 22:54:01 -0800

    remove test component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./Header";
 import ThreadController from "./ThreadController";
-// import SubredditData from "./SubredditData";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      {/* <SubredditData /> */}
       <ThreadController />
     </React.Fragment>
   );
