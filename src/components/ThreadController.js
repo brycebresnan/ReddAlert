b5e23e136739dbbeee58a2e4b37b3fca0cb396e3
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-04 22:16:46 -0800

    refactor class to use hooks, add getAuthToken

--- src/components/ThreadController.js
+++ src/components/ThreadController.js
@@ -1,48 +1,73 @@
 import ThreadList from "./ThreadList"
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NewThreadForm from "./NewThreadForm";
 
-class ThreadController extends React.Component {
+function ThreadController() {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      formVisibleOnPage: false,
-      mainThreadList: []
-    };
-  }
-
-  handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
-  }
-
-  handleAddingNewThreadToList = (newThread) => {
-    const newMainThreadList = this.state.mainThreadList.concat(newThread); 
-    this.setState({mainThreadList: newMainThreadList,
-                  formVisibleOnPage: false });
-  }
-
-  render(){
-    let currentlyVisisbleState = null;
-    let buttonText = null;
-
-    if (this.state.formVisibleOnPage) {
-      currentlyVisisbleState = <NewThreadForm onNewThreadCreation={this.handleAddingNewThreadToList}/>
-      buttonText = "Return to Thread List"
-    } else {
-      currentlyVisisbleState = <ThreadList threadList={this.state.mainThreadList} />
-      buttonText = "Add Thread"
-    }
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainThreadList, setMainThreadList] = useState([]);
+  const [token, setToken] = useState(null);
+  const [tokenError, setTokenError] = useState(null);
   
-    return(
-      <React.Fragment>
-        {currentlyVisisbleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </React.Fragment>
-    );
+
+  useEffect(() => {
+    const encodedKey = btoa(`${process.env.REACT_APP_APP_ID}:${process.env.REACT_APP_SECRET}`)
+
+    fetch(`https://www.reddit.com/api/v1/access_token`, {
+      method:"POST",
+      headers: {
+      'Authorization': `Basic ${encodedKey}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'grant_type': 'client_credentials'
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      } else {
+        return response.json()
+      }
+    })
+    .then((jsonifiedResponse) => {
+      setToken(jsonifiedResponse.access_token)
+      })
+    .catch((error) => {
+      setTokenError(error.message)
+    });
+  },[])
+  
+
+  const handleClick = () => {
+    setFormVisibleOnPage(!formVisibleOnPage)
+    };
+
+
+  const handleAddingNewThreadToList = (newThread) => {
+    const newMainThreadList = mainThreadList.concat(newThread); 
+    setMainThreadList(newMainThreadList);
+    setFormVisibleOnPage(false);
   }
+
+  let currentlyVisisbleState = null;
+  let buttonText = null;
+
+  if (formVisibleOnPage) {
+    currentlyVisisbleState = <NewThreadForm onNewThreadCreation={handleAddingNewThreadToList}/>
+    buttonText = "Return to Thread List"
+  } else {
+    currentlyVisisbleState = <ThreadList threadList={mainThreadList} />
+    buttonText = "Add Thread"
+  }
+
+  return(
+    <React.Fragment>
+      {currentlyVisisbleState}
+      <button onClick={handleClick}>{buttonText}</button>
+    </React.Fragment>
+  );
+  
 }
 
 export default ThreadController;
