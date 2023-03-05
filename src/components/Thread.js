db5e45acfa5e1245ad0921aee8b64bcaaf08cb61
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-05 15:48:59 -0800

    UPDATE var names

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 
 
 function Thread(props) {
-  const activePercent = (props.accountsActive / props.subscribers) * 10000
+  const activeScore = (props.accountsActive / props.subscribers) * 10000
   return (
     <React.Fragment>
       <h3>Subreddit name: {props.displayName} </h3>
       <h3>Members: {props.subscribers}</h3>
       <h3>Online: {props.accountsActive}</h3>
-      <h4>Active Score: {activePercent.toFixed(1)} </h4>
+      <h4>Active Score: {activeScore.toFixed(1)} </h4>
       <br></br>
     </React.Fragment>
   );
