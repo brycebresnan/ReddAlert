1ae29a4c8b13b7689bfe6833e501d7613531061a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-05 22:39:06 -0800

    update props in detail

--- src/components/ThreadDetail.js
+++ src/components/ThreadDetail.js
@@ -2,23 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ThreadDetail(props) {
-  const {thread} = props;
+  const {displayName, subscribers, accountsActive, activeScore, isHot, id } = props;
 
   return(
     <>
-      <h1>Thread Detail</h1>
-      <h3>Subreddit name: {thread.displayName} </h3>
-      <h3>Members: {thread.subscribers}</h3>
-      <h3>Online: {thread.accountsActive}</h3>
-      <h4>Active Score: {thread.activeScore} </h4>
+      <h3>Subreddit name: {displayName} </h3>
+      <h3>Members: {subscribers}</h3>
+      <h3>Online: {accountsActive}</h3>
+      <h4>Active Score: {activeScore.toFixed(1)} </h4>
+      <h4>Is it Hot? {isHot}</h4>
       <button onClcik={onClickingThreadSettings}>Settings</button>
-      <button onClick={() => onClickingDelete(thread.id)}>Delete</button>
+      <button onClick={() => onClickingDelete(id)}>Delete</button>
     </>
   )
 }
 
 ThreadDetail.propTypes = {
-  thread: PropTypes.object,
+  displayName: PropTypes.string,
+  subscribers: PropTypes.number,
+  accountsActive: PropTypes.number,
+  activeScore: PropTypes.number,
+  isHot: PropTypes.bool,
+  id: PropTypes.string,
   onClickingDelete: PropTypes.func,
   onClickingThreadSettings: PropTypes.func 
 };
