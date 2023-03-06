d5d7accb8c64ec5acfe6c329da3824c406b81cdd
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-05 18:43:55 -0800

    remove exess function after refactor

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -4,64 +4,10 @@ import PropTypes from "prop-types";
 import { v4 } from 'uuid';
 
 function ThreadList(props) {
-  const { threadList, token } = props;
+  const { threadList } = props;
 
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
-  const [tokenError, setTokenError] = useState(null);
-
-  
-
-  const handleMakeApiCall = (subreddit) => {
-    if (token == null) {
-      setTokenError("Authentication Token missing or undefined")
-      return
-    }
-    
-    fetch(`https://oauth.reddit.com/r/${subreddit}/about`, { headers: {Authorization: `Bearer ${token}`}})
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status}: ${response.statusText}`);
-      } else {
-        return response.json()
-      }
-    })
-    .then((jsonifiedResponse) => {
-      setData(prevArray  => [...prevArray, jsonifiedResponse.data])
-    })
-    .catch((error) => {
-      setError(error.message)
-    });
-  }
-
-  const apiCallLoop = (subredditList) => {
-    if (data.length == threadList.length) {
-      setData([])
-    }
-    subredditList.forEach(sub => {
-      handleMakeApiCall(sub.displayName);
-    });
-  }
-
-  // useState(apiCallLoop(props.threadList), [])
-
-  if (tokenError) {
-    return (
-    <>
-      <h1>Authentication Error: {tokenError}</h1>;
-    </>
-    )
-  } else if (error) {
-    return (
-      <>
-        <h1>API Error: {error}</h1>;
-        <button onClick={() => apiCallLoop(props.threadList)}>Refresh API Call</button>
-      </>
-    );
-  } else {
     return (
       <React.Fragment>
-        <button onClick={() => apiCallLoop(props.threadList)}>Refresh API Call</button>
           {threadList.map((thread) =>
             <Thread displayName={thread.displayName}
             accountsActive={thread.accountsActive}
@@ -73,7 +19,7 @@ function ThreadList(props) {
     );
   }
 
-}
+
 
 ThreadList.propTypes = {
   threadList: PropTypes.array,
