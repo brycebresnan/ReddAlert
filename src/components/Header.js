78ff345fd6d6285d5b5afbff48b2aa10084d8feb
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-08 20:42:50 -0800

    add siren icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { AiOutlineAlert } from "react-icons/ai";
+import {GiSiren} from "react-icons/gi" 
 
 function Header(){
   return(
     <React.Fragment>
-      <h1>ReddAlert</h1>
+      <h1>ReddAlert<GiSiren/></h1>
     </React.Fragment>
   );
 }
